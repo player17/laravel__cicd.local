7e9c15f3968e3273c720b3de02cef2ce9534b1c5
mtex-user <reandh@yandex.ru>
2024-12-28 01:58:45 +0500

    init docker-compose

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,14 @@ export default defineConfig({
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
+            refresh: true, // Автоматически обновлять страницу при изменении файлов
         }),
     ],
+    server: {
+        https: false, // Использовать HTTP (если HTTPS не требуется)
+        origin: 'http://docker.vm-32cfa24b.na4u.ru:8473', // Указываем базовый URL для генерации правильных ссылок
+        host: '0.0.0.0', // Разрешить доступ извне (важно для Docker)
+        port: 5173, // Порт, который Vite будет использовать внутри контейнера
+        strictPort: true, // Запретить автоматический выбор порта
+    },
 });
